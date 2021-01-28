602e1a792e9f803938b81541208ccb827806bed7
Marta919 <72815759+Marta919@users.noreply.github.com>
2021-01-28 18:34:51 +0100

    Adding Descriptions

---
+++ weather.js
@@ -0,0 +1,44 @@
+
+let time = new Date ();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+let day = days[time.getDay()];
+
+function date(time){
+
+let minutes = time.getMinutes();
+if (minutes<10){
+    minutes = (`0${minutes}`);
+}
+let hours = time.getHours();
+if (hours < 10){
+    hours = (`0${hours}`);
+}
+return `${day}, ${hours}:${minutes}`;
+}
+
+let now = document.querySelector("#time");
+now.innerHTML = date(time);
+
+
+
+
+
+
+
+
+
+
+function displayTemperature (response){
+let temperature = document.querySelector("#temp");
+let selectedCity = document.querySelector("#city");
+temperature.innerHTML = Math.round(response.data.main.temp);
+selectedCity.innerHTML = response.data.name;
+document.querySelector("#sky").innerHTML= response.data.weather[0].description;
+document.querySelector("#humidity").innerHTML= `Humidity:${Math.round(response.data.main.humidity)}%`;
+document.querySelector("#wind").innerHTML= `Wind Speed:${Math.round(response.data.wind.speed)}km/h`;
+}
+
+let apiKey = "e8cf93c11b2e03971616c05c042f7ad8"; 
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Helsinki&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
